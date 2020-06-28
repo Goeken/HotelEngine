e73f55d7d195edfa2162b6cce0e3620444ef72a7
Goeken <codygoeken@gmail.com>
2020-06-28 17:01:36 -0600

    added search controller and model

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,9 @@
 class SearchesController < ApplicationController
+	BASE = 'https://superheroapi.com/api/3407598565938592/'
+	# response = HTTParty.get('https://superheroapi.com/api/3407598565938592')
+
+	def search_name
+		return "hi"
+	end
+
 end
--- db/migrate/20200628000352_create_searches.rb
+++ db/migrate/20200628000352_create_searches.rb
@@ -1,7 +1,7 @@
 class CreateSearches < ActiveRecord::Migration[6.0]
   def change
     create_table :searches do |t|
-
+      t.string :search_text
       t.timestamps
     end
   end
