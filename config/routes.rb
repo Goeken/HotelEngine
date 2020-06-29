1d11323ab3a920ffd002f402156a99f136b62023
Goeken <codygoeken@gmail.com>
2020-06-29 11:26:24 -0600

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "searches#index"
   resources :searches, only: [:index]
   resources :movies, only: [:create, :update, :destroy]
 end
